95ae2a53af31b7ed423de7e9956f315ae256e58c
Sandra-0701 <sandrasindhu0701@gmail.com>
2024-05-16 11:46:55 +0530

    commit

---
+++ front-end/src/axiosInterceptor.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000', 
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = sessionStorage.getItem('userToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
